c60423a819180f9031a12af1747aecddda7509cc
Nina Baculinao <nb2406@columbia.edu>
2017-03-28 18:02:33 -0400

    Alexa recites more rhymes

--- alexa-skill-nursery-rhymes/src/index.js
+++ alexa-skill-nursery-rhymes/src/index.js
@@ -10,6 +10,15 @@
 var Alexa = require('alexa-sdk');
 var APP_ID = 'amzn1.ask.skill.ad861929-ecc8-415c-a441-8e750ac3e268';
 
+const sailorWentToSea = {
+    name: 'A Sailor Went To Sea',
+    lyrics:
+    'A sailor went to sea sea sea.\
+    To see what he could see see see.\
+    But all that he could see see see.\
+    Was the bottom of the deep blue sea sea sea!'
+};
+
 const fiveLittleMonkeys = {
     name: 'Five Little Monkeys',
     lyrics:
@@ -42,11 +51,31 @@ const fiveLittleMonkeys = {
     They\'re all jumping on the sofa instead!'
 };
 
+const littleTeapot = {
+    name: 'I\'m A Little Teapot',
+    lyrics:
+    'I\'m a little teapot.\
+    Short and stout.\
+    Here is my handle.\
+    Here is my spout.\
+    When I get all steamed up.\
+    Hear me shout.\
+    Just tip me over and pour me out!\
+    \
+    I\'m a special teapot.\
+    Yes, it\'s true.\
+    Here\'s an example of what I can do.\
+    I can turn my handle into a spout.\
+    Just tip me over and pour me out!'
+};
+
 const languageStrings = {
     'en': {
         'translation': {
             'RHYMES': [
+                sailorWentToSea,
                 fiveLittleMonkeys,
+                littleTeapot
             ],
             'SKILL_NAME' : 'Nursery Rhymes',
             'HELP_MESSAGE' : 'You can say "give me a nursery rhyme", or you can say "exit" this time... How do you do? What can I do for you?',
