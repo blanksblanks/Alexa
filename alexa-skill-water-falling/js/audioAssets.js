bbc60e170f45dfcbd96375ca1b55f2585efb6292
Nina Baculinao <nb2406@columbia.edu>
2017-04-04 14:23:15 -0400

    Add water falling take 2

---
+++ alexa-skill-water-falling/js/audioAssets.js
@@ -0,0 +1,47 @@
+'use strict';
+
+// Audio source: https://www.soundjay.com/nature/river-1.mp3
+var audioData = [
+    {
+        'title' : 'River 1',
+        'url' : 'https://www.soundjay.com/nature/river-1.mp3'
+    },
+    {
+        'title' : 'River 2',
+        'url' : 'https://www.soundjay.com/nature/river-1.mp3'
+    },
+    {
+        'title' : 'River 3',
+        'url' : 'https://www.soundjay.com/nature/river-1.mp3'
+    },
+    {
+        'title' : 'River 4',
+        'url' : 'https://www.soundjay.com/nature/river-1.mp3'
+    },
+    {
+        'title' : 'River 5',
+        'url' : 'https://www.soundjay.com/nature/river-1.mp3'
+    },
+    {
+        'title' : 'River 6',
+        'url' : 'https://www.soundjay.com/nature/river-1.mp3'
+    },
+    {
+        'title' : 'River 1',
+        'url' : 'https://www.soundjay.com/nature/river-1.mp3'
+    },
+    {
+        'title' : 'Stream 1',
+        'url' : 'https://www.soundjay.com/nature/stream-1.mp3'
+    },
+    {
+        'title' : 'Stream 2',
+        'url' : 'https://www.soundjay.com/nature/stream-2.mp3'
+    },
+    {
+        'title' : 'Stream 3',
+        'url' : 'https://www.soundjay.com/nature/stream-3.mp3'
+    }
+];
+
+module.exports = audioData;
